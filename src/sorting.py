dc328acf3a721fad7f8bd0d0e9aa4bc9677406a7
Sergey <smirsand@mail.ru>
2023-06-01 17:17:30 +0300

    "Файл sorting.py c aeyrwbtq принимает список и сортирует его"

---
+++ src/sorting.py
@@ -0,0 +1,14 @@
+from src.utils import read_operations
+
+
+def sorting_operations():
+    '''
+    Функция принимает список и сортирует по выполненным последним операциям. Последняя выполненная операция первая.
+    '''
+    operations_catd = [item for item in read_operations() if item.get("state") == "EXECUTED" and "date" in item]
+    operations_catd.sort(key=lambda x: x["date"], reverse=True)
+    print(operations_catd)
+    return operations_catd
+
+
+sorting_operations()
