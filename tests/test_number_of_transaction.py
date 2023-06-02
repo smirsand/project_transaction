bb357aa7f22f51992bc67b0f2fd66b37493ad789
Sergey <smirsand@mail.ru>
2023-06-02 15:19:37 +0300

    "Добавлено описание к функции test_number_of_transaction.py и изменен код теста"

--- tests/test_number_of_transaction.py
+++ tests/test_number_of_transaction.py
@@ -1,5 +1,10 @@
-from src import number_of_transaction
+from src.number_of_transaction import output
 
 
 def test_number_of_transaction():
-    assert number_of_transaction.output() == print(str)
+    """
+    Функция проверяет, является ли “value” None или строкой
+    :return:
+    """
+    value = output()
+    assert value is None or isinstance(value, str)
