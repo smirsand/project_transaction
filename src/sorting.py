4b04d1e482e9f7878c9b9ebdbdcf9fb8561ab13f
Sergey <smirsand@mail.ru>
2023-06-02 10:45:52 +0300

    "Кавычки догстрингов изменены на ' """ ' "

--- src/sorting.py
+++ src/sorting.py
@@ -2,9 +2,10 @@ from src.utils import read_operations
 
 
 def sorting_operations():
-    '''
-    Функция принимает список и сортирует по выполненным последним операциям. Последняя выполненная операция первая.
-    '''
+    """
+    Функция принимает список с данными и сортирует по успешно выполненным последним операциям,
+    и сортирует по дате. Последняя выполненная операция первая в списке.
+    """
     operations_catd = [item for item in read_operations() if item.get("state") == "EXECUTED" and "date" in item]
     operations_catd.sort(key=lambda x: x["date"], reverse=True)
     return operations_catd
