d05e3c0177f84a36e2356f2d235feb97d0e54352
Sergey <smirsand@mail.ru>
2023-06-02 11:30:12 +0300

    "Изменение переменной а на result"

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,13 +6,13 @@ def test_read_operations():
     Функция проводит тест на корректность чтения файла operations.json.
     Сравнивает типы функции read_operations() (файл utils.py) с типом результата.
     """
-    a = [{'date': '2019-08-26T10:50:58.294041',
-          'description': 'Перевод организации',
-          'from': 'Maestro 1596837868705199',
-          'id': 441945886,
-          'operationAmount': {'amount': '31957.58',
-                              'currency': {'code': 'RUB', 'name': 'руб.'}},
-          'state': 'EXECUTED',
-          'to': 'Счет 64686473678894779589'}]
+    result = [{'date': '2019-08-26T10:50:58.294041',
+               'description': 'Перевод организации',
+               'from': 'Maestro 1596837868705199',
+               'id': 441945886,
+               'operationAmount': {'amount': '31957.58',
+                                   'currency': {'code': 'RUB', 'name': 'руб.'}},
+               'state': 'EXECUTED',
+               'to': 'Счет 64686473678894779589'}]
 
-    assert type(utils.read_operations()) == type(a)
+    assert type(utils.read_operations()) == type(result)
